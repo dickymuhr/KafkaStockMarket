20acbf5564bc0974c339f22edef7b93a633a0dfb
Dicky Muhammad Rizal <dickymuhrizal@gmail.com>
2023-09-20 09:20:13 +0700

    dummy producer

--- KafkaProducer.py
+++ KafkaProducer.py
@@ -4,5 +4,13 @@ from time import sleep
 from json import dumps
 import json
 
-producer = KafkaProducer(bootstrap_servers=['16.171.153.102:9092'],
+producer = KafkaProducer(bootstrap_servers=['13.51.196.19:9093'],
                          value_serializer = lambda x: dumps(x).encode('utf-8'))
+                        # why do we need utf-8 encoding? because kafka need byte string, not regular string
+
+df = pd.read_csv('indexProcessed.csv')
+
+while True:
+    stock_dict = df.sample(1).to_dict(orient="records")[0]
+    producer.send("stock", value=stock_dict)
+    sleep(1)
